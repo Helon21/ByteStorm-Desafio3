dc121bbd1c594eed18dfb06886c926002ffac0e9
Matheus Laurentino <matheus.laurentino.ifpr@gmail.com>
2024-06-05 11:10:57 -0300

    commit inicial - msfuncionarios

---
+++ msfuncionarios/src/main/java/bytestorm/msfuncionarios/MsfuncionariosApplication.java
@@ -0,0 +1,13 @@
+package bytestorm.msfuncionarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsfuncionariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsfuncionariosApplication.class, args);
+	}
+
+}

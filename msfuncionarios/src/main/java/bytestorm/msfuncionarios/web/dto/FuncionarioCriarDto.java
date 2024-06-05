8f6e4ef3c598451ece77e3024d500a31c6293beb
Matheus Laurentino <matheus.laurentino.ifpr@gmail.com>
2024-06-05 15:08:46 -0300

    fix: dto funcionarios

--- msfuncionarios/src/main/java/bytestorm/msfuncionarios/web/dto/FuncionarioCriarDto.java
+++ msfuncionarios/src/main/java/bytestorm/msfuncionarios/web/dto/FuncionarioCriarDto.java
@@ -1,13 +1,13 @@
 package bytestorm.msfuncionarios.web.dto;
 
-import bytestorm.msfuncionarios.entity.Funcionario;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.antlr.v4.runtime.misc.NotNull;
 import org.hibernate.validator.constraints.br.CPF;
 
 import java.time.LocalDate;
@@ -17,18 +17,16 @@ import java.time.LocalDate;
 @Getter
 @Setter
 public class FuncionarioCriarDto {
-
-        @NotNull
-        @Size(min = 2, max = 100)
-        private String nome;
-        @Size(min = 11, max = 11)
-        @NotNull
-        @CPF
-        private String cpf;
-        @NotNull
-        LocalDate dataNascimento;
-        @NotNull
-        @Pattern(regexp = "MASCULINO|FEMININO")
-        private String sexo;
-
+    @NotBlank
+    @Size(min = 2, max = 100)
+    private String nome;
+    @Size(min = 11, max = 11)
+    @NotBlank
+    @CPF
+    private String cpf;
+    @NotNull
+    private LocalDate dataNascimento;
+    @NotBlank
+    @Pattern(regexp = "MASCULINO|FEMININO")
+    private String sexo;
 }

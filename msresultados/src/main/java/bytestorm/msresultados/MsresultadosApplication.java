bdcd88ff4ed299b677e4afe15be3a185c01e9394
Matheus Laurentino <matheus.laurentino.ifpr@gmail.com>
2024-06-06 16:09:08 -0300

    commit inicial msresultados

---
+++ msresultados/src/main/java/bytestorm/msresultados/MsresultadosApplication.java
@@ -0,0 +1,13 @@
+package bytestorm.msresultados;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsresultadosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsresultadosApplication.class, args);
+	}
+
+}
---
+++ msresultados/src/test/java/bytestorm/msresultados/MsresultadosApplicationTests.java
@@ -0,0 +1,13 @@
+package bytestorm.msresultados;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsresultadosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

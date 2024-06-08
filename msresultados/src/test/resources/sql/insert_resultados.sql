7a4e973ead948dfee4e9dc03ee017b39e8d40692
Matheus Laurentino <matheus.laurentino.ifpr@gmail.com>
2024-06-07 22:22:58 -0300

    test: testes controller e service

---
+++ msresultados/src/test/resources/sql/insert_resultados.sql
@@ -0,0 +1,7 @@
+INSERT INTO resultados (id, titulo, descricao, id_funcionario, data_votacao, qtd_aprovado, qtd_rejeitado, status) VALUES
+(1, 'Cinema na Comunidade', 'Projeto para construir um cinema comunitário para entretenimento local.', 1, '2023-05-10 14:30:00', 50, 10, 'APROVADO'),
+(2, 'Piscina Pública', 'Construção de uma nova piscina pública para lazer e atividades físicas.', 2, '2023-06-20 16:00:00', 45, 15, 'REJEITADO'),
+(3, 'Parque Verde', 'Criação de um parque verde para promover atividades ao ar livre e conservação ambiental.', 3, '2023-07-15 10:45:00', 60, 5, 'APROVADO'),
+(4, 'Biblioteca Digital', 'Desenvolvimento de uma biblioteca digital acessível para todos os cidadãos.', 4, '2023-08-25 13:00:00', 55, 12, 'REJEITADO'),
+(5, 'Centro Esportivo', 'Construção de um centro esportivo multifuncional para diversas modalidades.', 1, '2023-09-10 11:30:00', 65, 8, 'APROVADO'),
+(6, 'Feira Cultural', 'Organização de uma feira cultural anual para promover artes locais.', 2, '2023-10-05 15:15:00', 70, 4, 'APROVADO');

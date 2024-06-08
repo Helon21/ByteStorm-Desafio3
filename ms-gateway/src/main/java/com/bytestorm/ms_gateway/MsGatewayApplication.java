5581fd09fdf4ce6d534cbae64b055ee934edec5e
Helon21 <helonbentes51@hotmail.com>
2024-06-08 15:00:22 -0300

    feature: criacao e configuracao do ms-gateway

---
+++ ms-gateway/src/main/java/com/bytestorm/ms_gateway/MsGatewayApplication.java
@@ -0,0 +1,24 @@
+package com.bytestorm.ms_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class MsGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsGatewayApplication.class, args);
+	}
+
+	public RouteLocator rotas(RouteLocatorBuilder builder) {
+		return builder
+				.routes()
+				.route(r -> r.path("/propostas/**").uri("lb://ms-propostas"))
+				.build();
+	}
+
+}

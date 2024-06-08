7e3fc2807b5125b3fa1385c3c4fc6239cf3d916e
Helon21 <helonbentes51@hotmail.com>
2024-06-08 11:59:33 -0300

    feature: testando mapper e removendo imports nnao usados

--- ms-propostas/src/test/java/com/bytestorm/ms_propostas/web/controller/PropostaControllerTestes.java
+++ ms-propostas/src/test/java/com/bytestorm/ms_propostas/web/controller/PropostaControllerTestes.java
@@ -1,28 +1,22 @@
-package com.bytestorm.ms_propostas.controller;
+package com.bytestorm.ms_propostas.web.controller;
 
 import com.bytestorm.ms_propostas.entity.Proposta;
 import com.bytestorm.ms_propostas.exception.PropostaInativaException;
 import com.bytestorm.ms_propostas.exception.PropostaNaoEncontradaException;
 import com.bytestorm.ms_propostas.service.PropostaService;
-import com.bytestorm.ms_propostas.web.dto.PropostaRespostaDTO;
 import com.bytestorm.ms_propostas.web.dto.mapper.PropostaMapper;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.jdbc.Sql;
-import org.springframework.test.web.reactive.server.WebTestClient;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.List;
 
 import static com.bytestorm.ms_propostas.common.PropostaConstantes.*;
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
 import static org.mockito.ArgumentMatchers.any;

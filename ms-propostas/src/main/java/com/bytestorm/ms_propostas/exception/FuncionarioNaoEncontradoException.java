e97e9b6b02ceeaa6fb27649bc1efd935421b3583
Matheus Laurentino <matheus.laurentino.ifpr@gmail.com>
2024-06-08 18:23:13 -0300

    feature: exceptions necessarias

---
+++ ms-propostas/src/main/java/com/bytestorm/ms_propostas/exception/FuncionarioNaoEncontradoException.java
@@ -0,0 +1,8 @@
+package com.bytestorm.ms_propostas.exception;
+
+public class FuncionarioNaoEncontradoException extends RuntimeException{
+
+    public FuncionarioNaoEncontradoException(String mensagem) {
+        super(mensagem);
+    }
+}

1823bf26d65597ef53ff34de6a43ca865bc4985f
Matheus Laurentino <matheus.laurentino.ifpr@gmail.com>
2024-06-09 21:47:23 -0300

    feature: envio de mensagem ao kafka implementado

--- ms-propostas/src/main/java/com/bytestorm/ms_propostas/service/PropostaService.java
+++ ms-propostas/src/main/java/com/bytestorm/ms_propostas/service/PropostaService.java
@@ -1,15 +1,20 @@
 package com.bytestorm.ms_propostas.service;
 
 import com.bytestorm.ms_propostas.entity.Proposta;
+import com.bytestorm.ms_propostas.entity.Voto;
 import com.bytestorm.ms_propostas.exception.*;
 import com.bytestorm.ms_propostas.repository.PropostaRepository;
+import com.bytestorm.ms_propostas.repository.VotoRepository;
 import com.bytestorm.ms_propostas.web.dto.IniciarVotacaoDTO;
 import com.bytestorm.ms_propostas.web.dto.PropostaCriarDTO;
+import com.bytestorm.ms_propostas.web.dto.ResultadoDTO;
 import com.bytestorm.ms_propostas.web.dto.mapper.PropostaMapper;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.io.Serializable;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.concurrent.Executors;
@@ -21,12 +26,16 @@ import java.util.concurrent.TimeUnit;
 public class PropostaService {
 
     private final PropostaRepository propostaRepository;
+    private final VotoRepository votoRepository;
     private final FuncionarioService funcionarioService;
     private final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+    private final KafkaTemplate<String, Serializable> kafkaTemplate;
 
-    public PropostaService(PropostaRepository propostaRepository, FuncionarioService funcionarioService) {
+    public PropostaService(PropostaRepository propostaRepository, VotoRepository votoRepository, FuncionarioService funcionarioService, KafkaTemplate<String, Serializable> kafkaTemplate) {
         this.propostaRepository = propostaRepository;
+        this.votoRepository = votoRepository;
         this.funcionarioService = funcionarioService;
+        this.kafkaTemplate = kafkaTemplate;
     }
 
     @Transactional(readOnly = true)
@@ -73,7 +82,6 @@ public class PropostaService {
         configurarVotacao(proposta, tempoVotacaoMin);
 
         scheduler.schedule(() -> encerrarVotacao(proposta.getId()), tempoVotacaoMin, TimeUnit.MINUTES);
-        log.info("Votação da proposta com id " + id + " iniciada e será encerrada em " + tempoVotacaoMin + " minutos.");
 
         return propostaRepository.save(proposta);
     }
@@ -85,7 +93,9 @@ public class PropostaService {
         if (proposta.getStatus() == Proposta.Status.EM_VOTACAO) {
             proposta.setStatus(Proposta.Status.VOTACAO_ENCERRADA);
             propostaRepository.save(proposta);
-            log.info("Votação da proposta com id " + propostaId + " encerrada.");
+
+            ResultadoDTO dto = criarResultadoDTO(proposta);
+            kafkaTemplate.send("resultado-topico", dto);
         }
     }
 
@@ -94,6 +104,23 @@ public class PropostaService {
             throw new PropostaNaoPodeEntrarEmVotacao(
                     "Esta proposta está com status: '" + proposta.getStatus() + "' e por isso não pode entrar em votação");
         }
+    }
+
+    private ResultadoDTO criarResultadoDTO(Proposta proposta) {
+        Integer qtdAprovado = votoRepository.countAprovadosByPropostaId(proposta.getId());
+        Integer qtdRejeitado = votoRepository.countRejeitadosByPropostaId(proposta.getId());
+        String status = (qtdAprovado > qtdRejeitado) ? "APROVADO" : "REJEITADO";
+
+        return new ResultadoDTO(
+                proposta.getId(),
+                proposta.getTitulo(),
+                proposta.getDescricao(),
+                proposta.getFuncionarioId(),
+                proposta.getDataVotacao(),
+                qtdAprovado,
+                qtdRejeitado,
+                status
+        );
     }
 
     private void configurarVotacao(Proposta proposta, Integer tempoVotacaoMin) {

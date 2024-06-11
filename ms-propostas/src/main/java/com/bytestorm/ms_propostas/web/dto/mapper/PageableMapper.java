53d74725dc1c5bdbc5566f55813ba4685e11fdc9
Matheus Laurentino <matheus.laurentino.ifpr@gmail.com>
2024-06-10 22:43:04 -0300

    feature: dto pageable

---
+++ ms-propostas/src/main/java/com/bytestorm/ms_propostas/web/dto/mapper/PageableMapper.java
@@ -0,0 +1,16 @@
+package com.bytestorm.ms_propostas.web.dto.mapper;
+
+import com.bytestorm.ms_propostas.web.dto.PageableDto;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.data.domain.Page;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class PageableMapper {
+
+    public static PageableDto toDto(Page page) {
+        return new ModelMapper().map(page, PageableDto.class);
+    }
+
+}
